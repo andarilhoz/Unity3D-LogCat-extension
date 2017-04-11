34bfe95374f3cd054e2dac2cb778f4bb56704721
Krste Tashonov <tashonov@gmail.com>
2017-04-11 15:44:06 +0200

    Extracted FilterConfiguration

---
+++ FilterConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace LogCatExtension
+{
+	internal class FilterConfiguration
+	{
+		// Filters
+		public bool prefilterOnlyUnity = true;
+		public bool filterError = true;
+		public bool filterWarning = true;
+		public bool filterDebug = true;
+		public bool filterInfo = true;
+		public bool filterVerbose = true;
+		public string filterByString = String.Empty;
+	}
+}
