089221e101fa7825a2d268441c78ff2401e22ebd
Krste Tashonov <tashonov@gmail.com>
2017-04-11 16:20:39 +0200

    Refactored FilterLogList implementation for readability

--- LogCatFilter.cs
+++ LogCatFilter.cs
@@ -12,20 +12,50 @@ namespace LogCatExtension
 			// Filter
 			foreach (var logCatLog in fullLogList) {
 				bool shouldFilterByString = config.filterByString.Length > 1;
-				if( (!shouldFilterByString || logCatLog.Message.ToLower().Contains( config.filterByString.ToLower() ) ) &&
-					(!config.prefilterOnlyUnity || logCatLog.Message.Contains("Unity") )
-					&&
-					(  config.filterError && logCatLog.Type == 'E'
-						|| config.filterWarning && logCatLog.Type == 'W'
-						|| config.filterDebug && logCatLog.Type == 'D'
-						|| config.filterInfo && logCatLog.Type == 'I'
-						|| config.filterVerbose && logCatLog.Type == 'V' ) )
-				{
-					filterLogList.Add (logCatLog);
+				if (!shouldFilterByString || FilterByString (logCatLog, config.filterByString)) {
+					
+				} else {
+					continue;
 				}
+				if (!config.prefilterOnlyUnity || FilterByUnityString (logCatLog)) {
+
+				} else {
+					continue;
+				}
+				bool filtered = false;
+				if (filtered || (config.filterError && FilterByType (logCatLog, 'E'))) {
+					filtered = true;
+				} 
+				if (filtered || (config.filterWarning && FilterByType (logCatLog, 'W'))) {
+					filtered = true;
+				} 
+				if (filtered || (config.filterDebug && FilterByType (logCatLog, 'D'))) {
+					filtered = true;
+				} 
+				if (filtered || (config.filterInfo && FilterByType (logCatLog, 'I'))) {
+					filtered = true;
+				} 
+				if (filtered || (config.filterVerbose && FilterByType (logCatLog, 'V'))) {
+					filtered = true;
+				} 
+
+				if(filtered)
+					filterLogList.Add (logCatLog);
 			}
 					
 			return filterLogList;
+		}
+
+		private static bool FilterByString(LogCatLog log, string filterBy){
+			return log.Message.ToLower ().Contains (filterBy.ToLower ());
+		}
+
+		private static bool FilterByUnityString(LogCatLog log){
+			return log.Message.Contains ("Unity");
+		}
+
+		private static bool FilterByType(LogCatLog log, char type){
+			return log.Type == type;
 		}
 	}
 }
