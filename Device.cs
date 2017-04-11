b7495475fbe41bb88da65b998dfef6485d41aadd
Krste Tashonov <tashonov@gmail.com>
2017-04-11 15:50:35 +0200

    Stable WIP Version, Added device class for TODO: Add device dropdown

---
+++ Device.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace LogCatExtension
+{
+	internal class Device
+	{
+		public readonly string Name;
+		public readonly string ID;
+
+		public Device( string data )
+		{
+			ID = data.Substring(0, data.IndexOf(" "));
+			Name = data.Substring(data.LastIndexOf(":")).TrimStart();
+		}
+	}
+}
